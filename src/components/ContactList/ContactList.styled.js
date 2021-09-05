1004db240322890adb30900fc9b2937e4630a4d2
Aleksanrda Sluchevska <alaska3a@gmail.com>
2021-09-05 20:28:09 +0100

    add previous files

---
+++ src/components/ContactList/ContactList.styled.js
@@ -0,0 +1,28 @@
+import styled from "@emotion/styled";
+export const Span = styled.span`
+font-weight: 500;
+font-style: italic;
+font-size: 17px;
+`
+
+export const Button = styled.button`
+padding: 5px;
+border-radius: 5px;
+box-shadow: 3px 2px 3px rgb(85, 42, 85);
+font-size: 14px;
+font-weight:700;
+  
+`
+export const ContactItems = styled.li`
+display: flex;
+max-width: 450px;
+justify-content: space-between;
+
+margin-bottom:5px;
+margin-left:100px; 
+`
+
+
+export const ContainerItems = styled.ul`
+margin-top: 40px;
+`
